a4842b95a975a85dda81eae200c35e515a0ad186
Raveen Panditha <raveenpanditha8@gmail.com>
2021-06-13 18:49:01 +0530

    progress bar

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { projectStorage } from '../firebase/config';
 
-const useStorage = () => {
+const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
     const [error, setError] = useState(null);
     const [url, setUrl] = useState(null);
